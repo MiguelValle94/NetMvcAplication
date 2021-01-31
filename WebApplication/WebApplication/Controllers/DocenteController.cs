418372bf52548e9a1726b23fc18b613c54f3b0e0
miguelbachi5a <miguelbachi5a@gmail.com>
2021-01-31 12:18:54 +0100

    feat: Teachers files

---
+++ WebApplication/WebApplication/Controllers/DocenteController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication.Controllers
+{
+    public class DocenteController : Controller
+    {
+        // GET: Docente
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public JsonResult listTeachers()
+        {
+            PruebaDataContext bd = new PruebaDataContext();
+            var list = bd.Docente.Where(p => p.BHABILITADO.Equals(1))
+                .Select(p => new { p.NOMBRE, p.IIDDOCENTE, p.APPATERNO, p.APMATERNO, p.EMAIL }).ToList();
+            return Json(list, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
---
+++ WebApplication/WebApplication/Scripts/Docente.js
@@ -0,0 +1,40 @@
+﻿$.get("Docente/listTeachers", function (data) {
+    createList(data)
+})
+
+const createList = (data) => {
+    let content = ""
+    content += '<table class="table" id="table-docente">'
+    content += "<thead>"
+    content += "<tr>"
+    content += "<td>TeacherID<td>"
+    content += "<td>Nombre<td>"
+    content += "<td>Primer Apellido<td>"
+    content += "<td>Segundo Apellido<td>"
+    content += "<td>Contacto<td>"
+    content += "</tr>"
+    content += "</thead>"
+
+    content += "<tbody>"
+    for (let i = 0; i < data.length; i++) {
+        content += "<tr>"
+        content += `<td>${data[i].IIDOCENTE}</td>`
+        content += `<td></td>`
+        content += `<td>${data[i].NOMBRE}</td>`
+        content += `<td></td>`
+        content += `<td>${data[i].APPATERNO}</td>`
+        content += `<td></td>`
+        content += `<td>${data[i].APMATERNO}</td>`
+        content += `<td></td>`
+        content += `<td>${data[i].EMAIL}</td>`
+        content += "</tr>"
+    }
+    content += "</tbody>"
+    content += "</table>"
+
+    document.getElementById("div-table-docente").innerHTML = content
+
+    $("#table-curso").dataTable(
+        { searching: false }
+    )
+}
