3fb02670dfa1ab2c42ec8d3a1bb5344d85c04484
miguelbachi5a <miguelbachi5a@gmail.com>
2021-01-31 11:12:42 +0100

    feat: Alumno controller, mpdels and scripts

---
+++ WebApplication/WebApplication/Controllers/AlumnoController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication.Controllers
+{
+    public class AlumnoController : Controller
+    {
+        // GET: Alumno
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public JsonResult listAlumns()
+        {
+            PruebaDataContext bd = new PruebaDataContext();
+            var list = bd.Alumno.Where(p => p.BHABILITADO.Equals(1))
+                .Select(p => new { p.NOMBRE, p.IIDALUMNO, p.APPATERNO,p.APMATERNO, p.TELEFONOPADRE }).ToList();
+            return Json(list, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
