bd7ec5d98c04dde0e42cf54d5e39821734300a9b
miguelbachi5a <miguelbachi5a@gmail.com>
2021-02-07 11:18:41 +0100

    feat: retrieve GradoSeccion join

---
+++ WebApplication/WebApplication/Controllers/GradoSeccionController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication.Controllers
+{
+    public class GradoSeccionController : Controller
+    {
+        // GET: GradoSeccion
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public JsonResult listGradoSeccion()
+        {
+            PruebaDataContext bd = new PruebaDataContext();
+            var list = (from gradosec in bd.GradoSeccion
+                       join sec in bd.Seccion
+                       on gradosec.IIDSECCION equals sec.IIDSECCION
+                       join grad in bd.Grado
+                       on gradosec.IIDGRADO equals grad.IIDGRADO
+                       select new
+                       {
+                           gradosec.IID,
+                           NOMBREGRADO = grad.NOMBRE,
+                           NOMBRESECCION = sec.NOMBRE
+                       }).ToList();
+            return Json(list, JsonRequestBehavior.AllowGet);
+        }
+
+        public JsonResult recoverData(int id)
+        {
+            PruebaDataContext bd = new PruebaDataContext();
+            GradoSeccion list = bd.GradoSeccion.Where(p => p.IID.Equals(id)).First();
+            return Json(list, JsonRequestBehavior.AllowGet);
+        }
+
+        public JsonResult listSections ()
+        {
+            PruebaDataContext bd = new PruebaDataContext();
+            var list = bd.Seccion.Where(p => p.BHABILITADO.Equals(1))
+                .Select(p => new { p.NOMBRE, p.IIDSECCION }).ToList();
+
+            return Json(list, JsonRequestBehavior.AllowGet);
+        }
+
+        public JsonResult listGrades ()
+        {
+            PruebaDataContext bd = new PruebaDataContext();
+            var list = bd.Grado.Where(p => p.BHABILITADO.Equals(1))
+                .Select(p => new { p.NOMBRE, p.IIDGRADO }).ToList();
+
+            return Json(list, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
---
+++ WebApplication/WebApplication/Scripts/GradoSeccion.js
@@ -0,0 +1,42 @@
+﻿$.get("GradoSeccion/listGradoSeccion", (data) => {
+    createList(data)
+})
+
+const createList = (data) => {
+    let content = ""
+    content += '<table class="table" id="table-gradoseccion">'
+    content += "<thead>"
+    content += "<tr>"
+    content += "<td>ID GRADOSECCION</td>"
+    content += "<td>NOMBRE GRADO</td>"
+    content += "<td>NOMBRE SECCION/td>"
+    content += "</tr>"
+    content += "</thead>"
+
+    content += "<tbody>"
+    for (let i = 0; i < data.length; i++) {
+        content += "<tr>"
+        content += `<td>${data[i].IID}</td>`
+        content += `<td>${data[i].NOMBREGRADO}</td>`
+        content += `<td>${data[i].NOMBRESECCION}</td>`
+        content += `<td><button class='btn btn-primary' onclick='openModal(${data[i].IIDDOCENTE})' data-toggle='modal' data-target='#myModal'>E</button><button class='btn btn-danger' onclick='deleteRegister(${data[i].IIDDOCENTE})'>X</button></td>`
+        content += "</tr>"
+    }
+    content += "</tbody>"
+    content += "</table>"
+
+    document.getElementById("div-table-gradoseccion").innerHTML = content
+
+    $("#table-gradoseccion").dataTable(
+        { searching: false }
+    )
+}
+
+const populateCbo = (data, control) => {
+    let content
+    content += "<option value='0'>--Seleccione--</option>"
+    for (let i = 0; i < data.length; i++) {
+        content += `<option value="${data[i].IID}">${data[i].NOMBRE}</option>`
+    }
+    control.innerHTML = content
+}
